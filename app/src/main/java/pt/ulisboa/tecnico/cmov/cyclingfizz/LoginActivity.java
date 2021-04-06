e21dc12a67e8936d44b6304dcedde0402d7de638
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2021-04-06 19:09:06 +0100

    Login activity created

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cyclingfizz/LoginActivity.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.cmov.cyclingfizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
