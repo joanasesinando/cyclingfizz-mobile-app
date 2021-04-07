48baee66d67017f62bbd23383abef3f5c421c6f4
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2021-04-07 21:23:38 +0100

    Created sign up layout

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cyclingfizz/SignUpActivity.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.cmov.cyclingfizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
