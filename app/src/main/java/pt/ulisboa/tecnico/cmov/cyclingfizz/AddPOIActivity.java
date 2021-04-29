384365549f91cd68868332079a80a1e128165802
joanasesinando@tecnico.ulisboa.pt <joanasesinando@tecnico.ulisboa.pt>
2021-04-29 14:04:16 +0100

    Deleting photos to POI (started)

--- app/src/main/java/pt/ulisboa/tecnico/cmov/cyclingfizz/AddPOIActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cyclingfizz/AddPOIActivity.java
@@ -1,12 +1,17 @@
 package pt.ulisboa.tecnico.cmov.cyclingfizz;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,9 +19,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -24,10 +29,9 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.mapbox.geojson.Point;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.Objects;
-
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class AddPOIActivity extends AppCompatActivity {
 
@@ -40,6 +44,10 @@ public class AddPOIActivity extends AppCompatActivity {
 
     PathRecorder pathRecorder;
     Point coordPOI;
+
+    int totalImgs = 0;
+    int selectedImgs = 0;
+    boolean deletingImgs = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,33 +62,22 @@ public class AddPOIActivity extends AppCompatActivity {
         setInputs();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         try {
             if (requestCode == PICK_IMAGES && resultCode == RESULT_OK && data != null) {
-                GridLayout gallery = findViewById(R.id.new_poi_gallery);
-                final float scale = getResources().getDisplayMetrics().density;
-
-
                 for (int i = 0; i < data.getClipData().getItemCount(); i++) {
                     // Get URI
                     ClipData.Item item = data.getClipData().getItemAt(i);
                     Uri uri = item.getUri();
 
                     // Update view
-                    ImageView newImg = new ImageView(this);
-                    newImg.setImageURI(uri);
-                    GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-                    params.width = 0                                                                                                    ;
-                    params.height = (int) (100 * scale);
-
-                    params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
-                    params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
-                    newImg.setLayoutParams(params);
-
-                    newImg.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                    gallery.addView(newImg, params);
+                    addImageToGallery(uri);
+                    totalImgs++;
                 }
+                GridLayout gallery = findViewById(R.id.new_poi_gallery);
+                if (totalImgs > 0) gallery.setVisibility(View.VISIBLE);
 
             } else {
                 Toast.makeText(this, "No photos selected", Toast.LENGTH_LONG).show();
@@ -114,6 +111,125 @@ public class AddPOIActivity extends AppCompatActivity {
         // Set save btn click listener
         MaterialButton saveBtn = findViewById(R.id.save_poi);
         saveBtn.setOnClickListener(v -> savePOI());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void addImageToGallery(Uri uri) {
+        GridLayout gallery = findViewById(R.id.new_poi_gallery);
+        final float scale = getResources().getDisplayMetrics().density;
+
+        // Create wrapper
+        ConstraintLayout imgWrapper = new ConstraintLayout(this);
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.width = 0;
+        params.height = (int) (100 * scale);
+        params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+        params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+        imgWrapper.setLayoutParams(params);
+
+        // Create image
+        ImageView newImg = new ImageView(this);
+        newImg.setImageURI(uri);
+        newImg.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imgWrapper.addView(newImg);
+
+        // Create overlay (when selected)
+        LinearLayout overlay = new LinearLayout(this);
+        LinearLayout.LayoutParams overlayParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        overlay.setBackgroundColor(getColor(R.color.orange_500));
+        overlay.setAlpha(0.4f);
+        overlay.setVisibility(View.GONE);
+        imgWrapper.addView(overlay, overlayParams);
+
+        // Create checked icon (when selected)
+        ImageView icon = new ImageView(this);
+        icon.setImageResource(R.drawable.ic_round_check_circle_24);
+        icon.setPadding((int) (10 * scale), (int) (10 * scale), (int) (10 * scale), (int) (10 * scale));
+        icon.setColorFilter(getColor(R.color.white));
+        icon.setVisibility(View.GONE);
+        imgWrapper.addView(icon);
+
+        gallery.addView(imgWrapper, params);
+
+        // Set click listeners
+        imgWrapper.setOnLongClickListener(v -> {
+            Log.d(TAG, "long click");
+            if (!deletingImgs) toggleToolbar();
+            deletingImgs = true;
+            Log.d(TAG, String.valueOf(((ViewGroup) v).getChildCount()));
+            View overlayChild = ((ViewGroup) v).getChildAt(1);
+            if (overlayChild.getVisibility() == View.VISIBLE) deselectImg(v);
+            else if (overlayChild.getVisibility() == View.GONE) selectImg(v);
+            return true;
+        });
+
+        imgWrapper.setOnClickListener(v -> {
+            Log.d(TAG, "normal click");
+            if (deletingImgs) {
+                View overlayChild = ((ViewGroup) v).getChildAt(1);
+                if (overlayChild.getVisibility() == View.VISIBLE) deselectImg(v);
+                else if (overlayChild.getVisibility() == View.GONE) selectImg(v);
+            }
+        });
+    }
+
+    private void toggleToolbar() {
+        View toolbar = findViewById(R.id.new_poi_toolbar);
+        View selectItemsToolbar = findViewById(R.id.new_poi_select_items_toolbar);
+
+        if (toolbar.getVisibility() == View.VISIBLE) {
+            toolbar.setVisibility(View.GONE);
+            selectItemsToolbar.setVisibility(View.VISIBLE);
+
+            // Set selecting items top bar close btn click listener
+//            ((MaterialToolbar) selectItemsToolbar).setNavigationOnClickListener(v -> {
+//                selectedImgs = 0;
+//                deselectAllImgs();
+//            }); //FIXME
+
+        } else {
+            toolbar.setVisibility(View.VISIBLE);
+            selectItemsToolbar.setVisibility(View.GONE);
+        }
+    }
+
+    private void selectImg(View view) {
+        // Update counter
+        selectedImgs++;
+        View toolbar = findViewById(R.id.new_poi_select_items_toolbar);
+        MaterialToolbar topBar = toolbar.findViewById(R.id.topAppBar);
+        topBar.setTitle(selectedImgs + " selected");
+
+        // Update image view
+        for (int i = 1; i < ((ViewGroup) view).getChildCount(); i++) {
+            View child = ((ViewGroup) view).getChildAt(i);
+            child.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void deselectImg(View view) {
+        // Update counter
+        selectedImgs--;
+        View toolbar = findViewById(R.id.new_poi_select_items_toolbar);
+        MaterialToolbar topBar = toolbar.findViewById(R.id.topAppBar);
+        topBar.setTitle(selectedImgs + " selected");
+
+        // Update image view
+        for (int i = 1; i < ((ViewGroup) view).getChildCount(); i++) {
+            View child = ((ViewGroup) view).getChildAt(i);
+            child.setVisibility(View.GONE);
+        }
+    }
+
+    private void deselectAllImgs() {
+        GridLayout gallery = findViewById(R.id.new_poi_gallery);
+        for (int i = 0; i < ((ViewGroup) gallery).getChildCount(); i++) {
+            View imgWrapper = ((ViewGroup) gallery).getChildAt(i);
+            for (int j = 1; j < ((ViewGroup) imgWrapper).getChildCount(); j++) {
+                View child = ((ViewGroup) imgWrapper).getChildAt(j);
+                child.setVisibility(View.GONE);
+            }
+        }
     }
 
 
